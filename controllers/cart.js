ad6d2ed4611aa3bbae472dcb8632353d625480b7
TARUN GOYAL <goyaltarun584@gmail.com>
2023-03-07 02:40:23 +0530

    Adding comments and formatting code

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,10 +1,12 @@
-var cron = require('node-cron');
-var moment = require('moment');
+const cron = require('node-cron');
+const moment = require('moment');
 
-let createdAt_30mins= "0 0 * *";
-let createdAt_1day= "0 0 * *";
-let createdAt_3day= "0 0 * *";
+//Initializing timers for sending notifications
+let createdAt_30mins = "0 0 * *";
+let createdAt_1day = "0 0 * *";
+let createdAt_3day = "0 0 * *";
 
+//Schedulers for sending notifications
 let taskForHalfhour = cron.schedule(`${createdAt_30mins} *`, () => {
     console.log('sends notification after 30 minutes');
 });
@@ -15,18 +17,21 @@ let taskFor_3day = cron.schedule(`${createdAt_3day} *`, () => {
     console.log('sends notification after 3 days');
 });
 
+
+//This map will act as DB for this project.
 var productStatus = new Map();
 
-exports.addToCart = (req, res, next) => {
+//Function for handling abandoned checkout requests
+exports.abandonedCheckout = (req, res, next) => {
     let data = req.body;
-    let createdAt = data.created_at;
-    // let createdAt = d.toJSON().slice(0, 19).replace('T', ':');
-    createdAt_30mins = moment(createdAt).add(30,'minutes').format('mm hh DD MM');
-    createdAt_1day = moment(createdAt).add(1,'day').format('mm hh DD MM');
-    createdAt_3day = moment(createdAt).add(3,'days').format('mm hh DD MM');
+    let checkoutCreatedAt = data.created_at;
+    createdAt_30mins = moment(checkoutCreatedAt).add(30, 'minutes').format('mm hh DD MM');
+    createdAt_1day = moment(checkoutCreatedAt).add(1, 'day').format('mm hh DD MM');
+    createdAt_3day = moment(checkoutCreatedAt).add(3, 'days').format('mm hh DD MM');
     let productId = data.id;
     let customerId = data.customer.id;
-    productStatus.set(productId, {customerId:customerId,dateTime: createdAt});
+    productStatus.set(productId, { customerId: customerId, dateTime: checkoutCreatedAt });
+
     //process of sending notifications starts here
     if (productStatus.has(productId)) {
         taskForHalfhour.start();
@@ -36,9 +41,12 @@ exports.addToCart = (req, res, next) => {
     return res.json({ message: 'OK' });
 }
 
+//Function for handling order placing requests
 exports.orderProduct = (req, res, next) => {
     let productId = req.body.id;
     let customerId = req.body.customer.id;
+
+    //check if product is present in map and same customer is placing order
     if (productStatus.has(productId) && productStatus.get(productId).customerId == customerId) {
         productStatus.delete(productId);
         taskForHalfhour.stop();
@@ -46,5 +54,4 @@ exports.orderProduct = (req, res, next) => {
         taskFor_3day.stop();
     }
     res.status(200).send("Order Placed succesfully")
-
 }
--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cartController = require('../controllers/cart') ;
+const cartController = require('../controllers/cart');
 
 const router = express.Router();
 
-router.post('/addtocart',cartController.addToCart);
-router.post('/orderProduct',cartController.orderProduct);
+router.post('/addtocart', cartController.abandonedCheckout);
+router.post('/orderProduct', cartController.orderProduct);
 
 
 module.exports = router;
